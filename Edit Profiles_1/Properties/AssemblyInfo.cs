ebd1f30ca86f50c0ed4e6a1880af126807e5c0ad
SKYLINE2\CarlosMT <carlos.matheus@skyline.be>
2025-07-09 12:22:46 -0400

    Add "Edit Profiles" project and update dependencies
    - Introduced new project "Edit Profiles" with associated files.
    - Upgraded target framework from v4.6.2 to v4.8 for compatibility.
    - Added package reference for `Skyline.DataMiner.Utils.SecureCoding.Analyzers`.
    - Introduced `.editorconfig` for consistent coding styles.
    - Added `app.config` for assembly binding redirects.
    - Implemented `ProfileDialog` class for user interface in automation script.
    - Added `stylecop.json` for code style rules.
    - Updated `AssemblyInfo.cs` with relevant assembly metadata.
    - Structured `Edit Profiles_1.csproj` for library references and build configurations.

---
+++ Edit Profiles_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Edit Profiles_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Edit Profiles_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("091A5D6B-3DA4-47DC-ADDF-CC35F778BE26")]
+[assembly: AssemblyVersion("1.0.0.0")]
