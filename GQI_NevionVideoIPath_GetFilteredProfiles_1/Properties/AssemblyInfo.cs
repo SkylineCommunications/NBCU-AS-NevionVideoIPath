54bf2b4f83649cca618a423e7afe0807d8c6e1d8
SKYLINE2\CarlosMT <carlos.matheus@skyline.be>
2025-07-10 14:35:51 -0400

    Add NevionSharedUtils project and update framework
    - Introduced `NevionSharedUtils` for shared utility code.
    - Added `GQI_NevionVideoIPath_GetFilteredProfiles` project.
    - Upgraded target framework from `v4.6.2` to `v4.8`.
    - Reordered `UserProperties` in project files for consistency.
    - Updated folder structure for XML files under `Nevion VideoIPath/GQI`.
    - Created `GQI_NevionVideoIPath_GetFilteredProfiles.xml` for new automation script.
    - Added `.editorconfig` for coding style enforcement.
    - Introduced `stylecop.json` for StyleCop configuration.
    - Updated `AssemblyInfo.cs` with metadata for new project.
    - Added new classes and methods in `DomIds.cs` and `GQIUtils.cs`.
    - Created `NevionSharedUtils.projitems` for managing shared code.
    - Added `NevionSharedUtils.shproj` to define shared project structure.

---
+++ GQI_NevionVideoIPath_GetFilteredProfiles_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GQI_NevionVideoIPath_GetFilteredProfiles_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("GQI_NevionVideoIPath_GetFilteredProfiles_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("2ED265A5-D97A-470D-9188-90900AB49D85")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ NevionSharedUtils/GQIUtils.cs
@@ -0,0 +1,66 @@
+﻿namespace NevionSharedUtils
+{
+	using Skyline.DataMiner.Analytics.GenericInterface;
+	using Skyline.DataMiner.Net.Helper;
+	using Skyline.DataMiner.Net.Messages;
+
+	public class GQIUtils
+	{
+		public static object[][] GetTable(GQIDMS dms, LiteElementInfoEvent response, int tableId, string[] tableFilter)
+		{
+			var partialTableRequest = new GetPartialTableMessage
+			{
+				DataMinerID = response.DataMinerID,
+				ElementID = response.ElementID,
+				ParameterID = tableId,
+			};
+
+			if (tableFilter.IsNullOrEmpty())
+			{
+				partialTableRequest.Filters = new[] { "forceFullTable=true" };
+			}
+			else
+			{
+				partialTableRequest.Filters = tableFilter;
+			}
+
+			var messageResponse = dms.SendMessage(partialTableRequest) as ParameterChangeEventMessage;
+			if (messageResponse.NewValue.ArrayValue != null && messageResponse.NewValue.ArrayValue.Length > 0)
+			{
+				return BuildRows(messageResponse.NewValue.ArrayValue);
+			}
+			else
+			{
+				return new object[0][];
+			}
+		}
+
+		private static object[][] BuildRows(ParameterValue[] columns)
+		{
+			int length1 = columns.Length;
+			int length2 = 0;
+			if (length1 > 0)
+				length2 = columns[0].ArrayValue.Length;
+			object[][] objArray;
+			if (length1 > 0 && length2 > 0)
+			{
+				objArray = new object[length2][];
+				for (int index = 0; index < length2; ++index)
+					objArray[index] = new object[length1];
+			}
+			else
+			{
+				objArray = new object[0][];
+			}
+
+			for (int index1 = 0; index1 < length1; ++index1)
+			{
+				ParameterValue[] arrayValue = columns[index1].ArrayValue;
+				for (int index2 = 0; index2 < length2; ++index2)
+					objArray[index2][index1] = arrayValue[index2].IsEmpty ? (object)null : arrayValue[index2].ArrayValue[0].InteropValue;
+			}
+
+			return objArray;
+		}
+	}
+}
