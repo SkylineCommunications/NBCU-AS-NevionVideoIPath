06e9da5c830810324fcb0b6d56f9a09622035287
Bautista Solanet <bautista.solanet@skyline.be>
2025-08-22 08:09:15 -0400

    Adding misc files

---
+++ Cleanup TAG Audio Task_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cleanup TAG Audio Task_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Cleanup TAG Audio Task_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("86B9FB7F-7A72-4C50-B27C-98879D724197")]
+[assembly: AssemblyVersion("1.0.0.0")]
