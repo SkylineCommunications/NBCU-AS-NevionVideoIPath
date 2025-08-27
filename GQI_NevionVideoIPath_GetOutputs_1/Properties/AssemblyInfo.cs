b78edb508d1892b9a007f91e83004ede2e086d33
Bautista Solanet <bautista.solanet@skyline.be>
2025-08-27 09:01:49 -0400

    Adding GetOutputs script

---
+++ GQI_NevionVideoIPath_GetOutputs_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GQI_NevionVideoIPath_GetOutputs_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("GQI_NevionVideoIPath_GetOutputs_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("E6031066-83B4-45CD-A27A-13774816BCCC")]
+[assembly: AssemblyVersion("1.0.0.0")]
